170074a4690803a5d764a8a7b830f6522147258a
Ethan Zeronik <ejzeronik@gmail.com>
2022-11-02 11:20:39 -0400

    basic structure setup

---
+++ Lab Seven/worksheet.sql
@@ -0,0 +1,142 @@
+/*
+Ethan Zeronik
+CNIT372 Lab Section #4
+Estimated Time Spent: 0hrs
+Late hours used: 0
+*/
+
+-- defaults to on
+SET verify off;
+/**/
+
+-- TODO: Question #1a
+
+/* Question #1a Query Results
+
+*/
+
+-- TODO: Question #1b
+
+/* Question #1b Query Results
+
+*/
+
+-- TODO: Question #1c
+
+/* Question #1c Query Results
+
+*/
+
+-- TODO: Question #1d
+
+/* Question #1d Query Results
+
+*/
+
+-- TODO: Question #1e
+
+/* Question #1e Query Results
+
+*/
+
+-- TODO: Question #1f
+
+/* Question #1f Query Results
+
+*/
+
+-- TODO: Question #2a
+
+/* Question #2a Query Results
+
+*/
+
+-- TODO: Question #2b
+
+/* Question #2b Query Results
+
+*/
+
+-- TODO: Question #2c
+
+/* Question #2c Query Results
+
+*/
+
+-- TODO: Question #2d
+
+/* Question #2d Query Results
+
+*/
+
+-- TODO: Question #2e
+
+/* Question #2e Query Results
+
+*/
+
+-- TODO: Question #2f
+
+/* Question #2f Query Results
+
+*/
+
+-- TODO: Question #2g
+
+/* Question #2g Query Results
+
+*/
+
+-- TODO: Question #2h
+
+/* Question #2h Query Results
+
+*/
+
+-- TODO: Question #2i
+
+/* Question #2i Query Results
+
+*/
+
+-- TODO: Question #3
+
+/* Question #3 Query Results
+
+*/
+
+-- TODO: Question #3a
+
+/* Question #3a Query Results
+
+*/
+
+-- TODO: Question #3b
+
+/* Question #3b Query Results
+
+*/
+
+-- TODO: Question #3c
+
+/* Question #3c Query Results
+
+*/
+
+-- TODO: Question #3d
+
+/* Question #3d Query Results
+
+*/
+
+-- TODO: Question #3e
+
+/* Question #3e Query Results
+
+*/
+
+-- TODO: Question #3f
+
+/* Question #3f Query Results
+
+*/
