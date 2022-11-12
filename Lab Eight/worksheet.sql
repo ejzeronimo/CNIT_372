f1cb5d9122f1d4147d688ce4864c4713d332b1d1
Ethan Zeronik <ejzeronik@gmail.com>
2022-11-12 14:18:59 -0500

    group part done

---
+++ Lab Eight/worksheet.sql
@@ -0,0 +1,148 @@
+/*
+Ethan Zeronik
+CNIT372 Lab Section #4
+Estimated Time Spent: ?hr
+Late hours used: 0
+*/
+
+-- defaults to on
+SET verify off;
+/**/
+
+-- TODO: Question #a1
+
+/* Question #a1 Query Results
+
+*/
+
+-- TODO: Question #a2
+
+/* Question #a2 Query Results
+
+*/
+
+-- TODO: Question #a3
+
+/* Question #a3 Query Results
+
+*/
+
+-- TODO: Question #a4
+
+/* Question #a4 Query Results
+
+*/
+
+-- TODO: Question #a5
+
+/* Question #a5 Query Results
+
+*/
+
+-- TODO: Question #a6
+
+/* Question #a6 Query Results
+
+*/
+
+-- TODO: Question #a7
+
+/* Question #a7 Query Results
+
+*/
+
+-- TODO: Question #a8
+
+/* Question #a8 Query Results
+
+*/
+
+-- TODO: Question #a9
+
+/* Question #a9 Query Results
+
+*/
+
+-- TODO: Question #a10
+
+/* Question #a10 Query Results
+
+*/
+
+-- TODO: Question #a11
+
+/* Question #a11 Query Results
+
+*/
+
+-- TODO: Question #a12
+
+/* Question #a12 Query Results
+
+*/
+
+-- TODO: Question #a13
+
+/* Question #a13 Query Results
+
+*/
+
+-- TODO: Question #a14
+
+/* Question #a14 Query Results
+
+*/
+
+-- TODO: Question #a15
+
+/* Question #a15 Query Results
+
+*/
+
+-- TODO: Question #a16
+
+/* Question #a16 Query Results
+
+*/
+
+-- TODO: Question #a17
+
+/* Question #a17 Query Results
+
+*/
+
+-- TODO: Question #a18
+
+/* Question #a18 Query Results
+
+*/
+
+-- TODO: Question #a19
+
+/* Question #a19 Query Results
+
+*/
+
+-- TODO: Question #a20
+
+/* Question #a20 Query Results
+
+*/
+
+-- TODO: Question #a21
+
+/* Question #a21 Query Results
+
+*/
+
+-- TODO: Question #a22
+
+/* Question #a22 Query Results
+
+*/
+
+-- TODO: Question #a23
+
+/* Question #a23 Query Results
+
+*/
